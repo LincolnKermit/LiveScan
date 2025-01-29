13d5a8aab267249493e4f15c84e31f48563f6a99
Lincoln Kermit <104798220+LincolnKermit@users.noreply.github.com>
2025-01-29 09:04:51 +0100

    Removed unnecessary function

--- Server/app.py
+++ Server/app.py
@@ -14,25 +14,12 @@ def read_csv(file_path):
             print(row[8], row[13], row[1], row[2], row[0])
 
 
-""" ------ Unfinished work ------- 
-def get_latest_coordinates(file_path):
-    coordinates = []
-    with open(file_path, 'r') as f:
-        coordinates = json.load(f)
-    except (FileNotFoundError, json.JSONDecodeError):
-        coordinates = []
-    return coordinates
-"""
-
-
-
 @app.route('/upload_gps', methods=['POST'])
 def upload_gps():
     data = request.get_json()
     longitude = data.get('longitude')
     latitude = data.get('latitude')
     timestamp = datetime.now().strftime('%H:%M:%S')
-
     with open('gps.csv', 'a') as csvfile:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow([longitude, latitude, timestamp])
